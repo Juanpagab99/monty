e1e0bac987318b06a77f0159a53979d8dd65613e
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-15 02:15:25 +0000

    mera vuelta

--- functions2.c
+++ functions2.c
@@ -9,13 +9,14 @@
 */
 void _pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp = *stack;
+	stack_t *tmp;
 
 	if (*stack == NULL || stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	tmp = *stack;
 	*stack = (*stack)->next;
 	free(tmp);
 }
