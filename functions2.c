a45d0ccb6034f5d21192778d41535f168315b179
Juan Pablo Gaviria <juanpagab99@gmail.com>
2021-05-15 02:56:19 +0000

    marque bien, marque bien

--- functions2.c
+++ functions2.c
@@ -59,7 +59,7 @@ void _add(stack_t **stack, unsigned int line_number)
 	stack_t *tmp;
 	int sum;
 
-	if (*stack == NULL || stack == NULL)
+	if (*stack == NULL || stack == NULL || !(stack)->next)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
