746d28b2a26ed8d018985f2d0e516966bed0545f
dontstopmeowing <dnny419@gmail.com>
2021-05-15 07:45:01 +0700

    Adding pop function

--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ void free_s(stack_t *stack);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
